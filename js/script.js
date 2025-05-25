7aa1fc720d0d0f7897a851c94eee477956bc17cb
JuliAlchemDev <julialchem@gmail.com>
2025-05-25 08:24:53 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,380 @@
+const catalogo = [
+  {
+    ref: '48',
+    tipo: 'cónicas',
+    grosorCono: 'K',
+    grosorPunta1: 3,
+    grosorPunta2: 1,
+    metal: 'oro',
+    forma: 'inclinadas'
+  },
+  {
+    ref: '49',
+    tipo: 'cónicas',
+    grosorCono: 'K',
+    grosorPunta1: 4,
+    grosorPunta2: 1.5,
+    metal: 'oro',
+    forma: 'inclinadas'
+  },
+  {
+    ref: '50',
+    tipo: 'cónicas',
+    grosorCono: 'K',
+    grosorPunta1: 2,
+    grosorPunta2: 0.5,
+    metal: 'oro',
+    forma: 'inclinadas'
+  },
+  {
+    ref: '45',
+    tipo: 'cónicas',
+    grosorCono: 'K',
+    grosorPunta1: 3,
+    grosorPunta2: 1,
+    metal: 'oro',
+    forma: 'rectas'
+  },
+  {
+    ref: '46',
+    tipo: 'cónicas',
+    grosorCono: 'K',
+    grosorPunta1: 4,
+    grosorPunta2: 1.5,
+    metal: 'oro',
+    forma: 'rectas'
+  },
+  {
+    ref: '47',
+    tipo: 'cónicas',
+    grosorCono: 'K',
+    grosorPunta1: 2,
+    grosorPunta2: 0.5,
+    metal: 'oro',
+    forma: 'rectas'
+  },
+  {
+    ref: '69',
+    tipo: 'cónicas',
+    grosorCono: 'K',
+    grosorPunta1: 3,
+    grosorPunta2: 1,
+    metal: 'inox',
+    forma: 'inclinadas'
+  },
+  {
+    ref: '70',
+    tipo: 'cónicas',
+    grosorCono: 'K',
+    grosorPunta1: 4,
+    grosorPunta2: 1.5,
+    metal: 'inox',
+    forma: 'inclinadas'
+  },
+  {
+    ref: '71',
+    tipo: 'cónicas',
+    grosorCono: 'K',
+    grosorPunta1: 2,
+    grosorPunta2: 0.5,
+    metal: 'inox',
+    forma: 'inclinadas'
+  },
+  {
+    ref: '72',
+    tipo: 'cónicas',
+    grosorCono: 'K',
+    grosorPunta1: 3,
+    grosorPunta2: 1,
+    metal: 'inox',
+    forma: 'rectas'
+  },
+  {
+    ref: '73',
+    tipo: 'cónicas',
+    grosorCono: 'K',
+    grosorPunta1: 4,
+    grosorPunta2: 1.5,
+    metal: 'inox',
+    forma: 'rectas'
+  },
+  {
+    ref: '74',
+    tipo: 'cónicas',
+    grosorCono: 'K',
+    grosorPunta1: 2,
+    grosorPunta2: 0.5,
+    metal: 'inox',
+    forma: 'rectas'
+  },
+
+  {
+    ref: 'F48',
+    tipo: 'cónicas',
+    grosorCono: 'F',
+    grosorPunta1: 3,
+    grosorPunta2: 1,
+    metal: 'oro',
+    forma: 'inclinadas'
+  },
+  {
+    ref: 'F49',
+    tipo: 'cónicas',
+    grosorCono: 'F',
+    grosorPunta1: 4,
+    grosorPunta2: 1.5,
+    metal: 'oro',
+    forma: 'inclinadas'
+  },
+  {
+    ref: 'F50',
+    tipo: 'cónicas',
+    grosorCono: 'F',
+    grosorPunta1: 2,
+    grosorPunta2: 0.5,
+    metal: 'oro',
+    forma: 'inclinadas'
+  },
+  {
+    ref: 'F45',
+    tipo: 'cónicas',
+    grosorCono: 'F',
+    grosorPunta1: 3,
+    grosorPunta2: 1,
+    metal: 'oro',
+    forma: 'rectas'
+  },
+  {
+    ref: 'F46',
+    tipo: 'cónicas',
+    grosorCono: 'F',
+    grosorPunta1: 4,
+    grosorPunta2: 1.5,
+    metal: 'oro',
+    forma: 'rectas'
+  },
+  {
+    ref: 'F47',
+    tipo: 'cónicas',
+    grosorCono: 'F',
+    grosorPunta1: 2,
+    grosorPunta2: 0.5,
+    metal: 'oro',
+    forma: 'rectas'
+  },
+  {
+    ref: 'F69',
+    tipo: 'cónicas',
+    grosorCono: 'F',
+    grosorPunta1: 3,
+    grosorPunta2: 1,
+    metal: 'inox',
+    forma: 'inclinadas'
+  },
+  {
+    ref: 'F70',
+    tipo: 'cónicas',
+    grosorCono: 'F',
+    grosorPunta1: 4,
+    grosorPunta2: 1.5,
+    metal: 'inox',
+    forma: 'inclinadas'
+  },
+  {
+    ref: 'F71',
+    tipo: 'cónicas',
+    grosorCono: 'F',
+    grosorPunta1: 2,
+    grosorPunta2: 0.5,
+    metal: 'inox',
+    forma: 'inclinadas'
+  },
+  {
+    ref: 'F72',
+    tipo: 'cónicas',
+    grosorCono: 'F',
+    grosorPunta1: 3,
+    grosorPunta2: 1,
+    metal: 'inox',
+    forma: 'rectas'
+  },
+  {
+    ref: 'F73',
+    tipo: 'cónicas',
+    grosorCono: 'F',
+    grosorPunta1: 4,
+    grosorPunta2: 1.5,
+    metal: 'inox',
+    forma: 'rectas'
+  },
+  {
+    ref: 'F74',
+    tipo: 'cónicas',
+    grosorCono: 'F',
+    grosorPunta1: 2,
+    grosorPunta2: 0.5,
+    metal: 'inox',
+    forma: 'rectas'
+  },
+
+  {
+    ref: '04',
+    tipo: '2 piezas',
+    grosorCono: 'K',
+    grosorPunta1: 3,
+    grosorPunta2: 1,
+    metal: 'inox',
+    forma: 'inclinadas'
+  },
+  {
+    ref: '05',
+    tipo: '2 piezas',
+    grosorCono: 'K',
+    grosorPunta1: 4,
+    grosorPunta2: 1.5,
+    metal: 'inox',
+    forma: 'inclinadas'
+  },
+  {
+    ref: '06',
+    tipo: '2 piezas',
+    grosorCono: 'K',
+    grosorPunta1: 2,
+    grosorPunta2: 0.5,
+    metal: 'inox',
+    forma: 'inclinadas'
+  },
+  {
+    ref: '22',
+    tipo: '2 piezas',
+    grosorCono: 'K',
+    grosorPunta1: 3,
+    grosorPunta2: 1,
+    metal: 'inox',
+    forma: 'rectas'
+  },
+  {
+    ref: '51',
+    tipo: '2 piezas',
+    grosorCono: 'K',
+    grosorPunta1: 4,
+    grosorPunta2: 1.5,
+    metal: 'inox',
+    forma: 'rectas'
+  },
+  {
+    ref: '23',
+    tipo: '2 piezas',
+    grosorCono: 'K',
+    grosorPunta1: 2,
+    grosorPunta2: 0.5,
+    metal: 'inox',
+    forma: 'rectas'
+  },
+  {
+    ref: 'F04',
+    tipo: '2 piezas',
+    grosorCono: 'F',
+    grosorPunta1: 3,
+    grosorPunta2: 1,
+    metal: 'inox',
+    forma: 'inclinadas'
+  },
+  {
+    ref: 'F05',
+    tipo: '2 piezas',
+    grosorCono: 'F',
+    grosorPunta1: 4,
+    grosorPunta2: 1.5,
+    metal: 'inox',
+    forma: 'inclinadas'
+  },
+  {
+    ref: 'F06',
+    tipo: '2 piezas',
+    grosorCono: 'F',
+    grosorPunta1: 2,
+    grosorPunta2: 0.5,
+    metal: 'inox',
+    forma: 'inclinadas'
+  },
+  {
+    ref: 'F22',
+    tipo: '2 piezas',
+    grosorCono: 'F',
+    grosorPunta1: 3,
+    grosorPunta2: 1,
+    metal: 'inox',
+    forma: 'rectas'
+  },
+  {
+    ref: 'F51',
+    tipo: '2 piezas',
+    grosorCono: 'F',
+    grosorPunta1: 4,
+    grosorPunta2: 1.5,
+    metal: 'inox',
+    forma: 'rectas'
+  },
+  {
+    ref: 'F23',
+    tipo: '2 piezas',
+    grosorCono: 'F',
+    grosorPunta1: 2,
+    grosorPunta2: 0.5,
+    metal: 'inox',
+    forma: 'rectas'
+  }
+]
+
+const search = document.getElementById('search')
+const content = document.getElementById('content')
+const description = catalogo.map((needle) => {
+  return `${needle.ref} | ${needle.tipo} ${needle.grosorCono}${needle.grosorPunta1} (${needle.grosorPunta2}) ${needle.metal} ${needle.forma}`.toUpperCase()
+})
+
+search.addEventListener('input', () => {
+  const input = search.value.toUpperCase().trim().split(' ').filter(Boolean)
+
+  const results = description.filter((desc) =>
+    input.every((world) => desc.includes(world))
+  )
+
+  const fragment = document.createDocumentFragment()
+
+  results.forEach((result) => {
+    const p = document.createElement('p')
+    p.textContent = `REF: ${result}`
+    fragment.appendChild(p)
+    const bntsSection = document.createElement('div')
+    p.appendChild(bntsSection)
+
+    const stikerBtn = document.createElement('button')
+    stikerBtn.className = 'stiker-btn'
+    bntsSection.appendChild(stikerBtn)
+
+    stikerBtn.addEventListener('click', () => {
+      // Creamos parámetros en la URL
+      const params = new URLSearchParams({
+        product: result
+      })
+      window.location.href = `/assets/sticker.html?${params.toString()}`
+    })
+
+    const boxBtn = document.createElement('button')
+    boxBtn.className = 'box-btn'
+    boxBtn.innerText = 'WYC'
+    bntsSection.appendChild(boxBtn)
+
+    boxBtn.addEventListener('click', () => {
+      // Creamos parámetros en la URL
+      const params = new URLSearchParams({
+        product: result
+      })
+      window.location.href = `/assets/box.html?${params.toString()}`
+    })
+  })
+
+  content.innerHTML = '' // Limpiar antes
+  content.appendChild(fragment)
+})
